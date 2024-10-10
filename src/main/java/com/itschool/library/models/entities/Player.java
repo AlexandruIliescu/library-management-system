a6853d77ea2d75f5bd31964be23deac63a1bf320
alexiliescu <alexandruiliescu19@gmail.com>
2024-10-10 18:24:41 +0300

    SOLID

---
+++ src/main/java/com/itschool/library/models/entities/Player.java
@@ -0,0 +1,12 @@
+package com.itschool.library.models.entities;
+
+import java.time.LocalDateTime;
+
+public class Player {
+
+    String firstName;
+    String lastName;
+    LocalDateTime lastHealthCheck;
+    String position;
+    int score;
+}
