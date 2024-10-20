ec0023701000f2b15e17150ba48e7b693f3abd7b
alexiliescu <alexandruiliescu19@gmail.com>
2024-10-20 21:37:13 +0300

    Cleanup

--- src/main/java/com/itschool/library/controllers/BookController.java
+++ src/main/java/com/itschool/library/controllers/BookController.java
@@ -3,13 +3,8 @@ package com.itschool.library.controllers;
 import com.itschool.library.models.dtos.CopiesAvailableDTO;
 import com.itschool.library.models.dtos.RequestBookDTO;
 import com.itschool.library.models.dtos.ResponseBookDTO;
-import com.itschool.library.models.entities.Book;
 import com.itschool.library.services.BookService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
